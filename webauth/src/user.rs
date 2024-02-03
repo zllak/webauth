505c9c8e98f5640005b68ce3d2387e793cd1ebe6
Thomas Meson <zllak@hycik.org>
2024-02-03 04:54:34 +0100

    webauth/user: add UserManager that protects paths by requiring a valid User

--- webauth/src/user.rs
+++ webauth/src/user.rs
@@ -1,4 +1,8 @@
-use std::fmt::Debug;
+use crate::session::Session;
+use http::{Request, Response};
+use serde::Deserialize;
+use std::{fmt::Debug, future::Future, pin::Pin};
+use tower_service::Service;
 
 /// A User which can be authenticated and identified.
 pub trait AuthUser: Debug + Clone {
@@ -18,18 +22,99 @@ where
     }
 }
 
-// /// An authentication backend
-// pub trait AuthBackend {
-//     type User: AuthUser;
-//     type Credentials;
-//     type Error: std::error::Error;
-//
-//     fn authenticate(
-//         &self,
-//         credentials: Self::Credentials,
-//     ) -> impl Future<Output = Result<Option<Self::User>, Self::Error>> + Send;
-//     fn get_user(
-//         &self,
-//         id: &<Self::User as AuthUser>::Id,
-//     ) -> impl Future<Output = Result<Option<Self::User>, Self::Error>> + Send;
-// }
+#[derive(Debug, Clone)]
+pub struct UserManager<Service, User, Store>
+where
+    Store: crate::store::Store<Object = User, Id = <User as AuthUser>::Id>,
+    User: AuthUser,
+{
+    inner: Service,
+    store: Store,
+}
+
+impl<ReqBody, ResBody, S, User, Store> Service<Request<ReqBody>> for UserManager<S, User, Store>
+where
+    S: Service<Request<ReqBody>, Response = Response<ResBody>> + Clone + Send + 'static,
+    S::Future: Send,
+    ReqBody: Send + 'static,
+    ResBody: Default,
+    User: AuthUser + Send + Sync + 'static,
+    for<'de> <User as AuthUser>::Id: Send + std::fmt::Debug + Deserialize<'de>,
+    Store: crate::store::Store<Object = User, Id = <User as AuthUser>::Id> + Clone + Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future =
+        Pin<Box<dyn Future<Output = std::result::Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
+        // https://docs.rs/tower/latest/tower/trait.Service.html#be-careful-when-cloning-inner-services
+        let clone = self.inner.clone();
+        let mut inner = std::mem::replace(&mut self.inner, clone);
+        let store = self.store.clone();
+
+        fn return_error<ResBody: Default, Error>(
+            code: http::StatusCode,
+        ) -> std::result::Result<Response<ResBody>, Error> {
+            let mut res = Response::default();
+            // TODO: headers
+            *res.status_mut() = code;
+            Ok(res)
+        }
+
+        Box::pin(async move {
+            // Start by getting the session
+            let Some(session) = req.extensions().get::<Session>() else {
+                // this should not be possible but we are in a protected space,
+                // redirect
+                tracing::warn!("not session found");
+                return return_error(http::StatusCode::UNAUTHORIZED);
+            };
+
+            // Get the user_uid from the session
+            let user_uid = match session.get::<<User as AuthUser>::Id>("user_uid") {
+                Ok(Some(user_uid)) => user_uid,
+                Ok(None) => {
+                    // Session not authenticated
+                    tracing::warn!(suid = %session.uid(), "no user_uid found in session");
+                    return return_error(http::StatusCode::UNAUTHORIZED);
+                }
+                Err(err) => {
+                    // Unable to get the user_uid from the session
+                    tracing::warn!(err = %err, suid = %session.uid(), "unable to get user_uid from session");
+                    return return_error(http::StatusCode::INTERNAL_SERVER_ERROR);
+                }
+            };
+
+            // Get the user
+            let user = match store.load(&user_uid).await {
+                Ok(Some(user)) => user,
+                Ok(None) => {
+                    // We have a valid session, with a user_uid that does not
+                    // resolve to a valid user. This should not happen.
+                    tracing::warn!(uid = %session.uid(), user_uid = ?user_uid, "unable to resolve a valid user");
+                    return return_error(http::StatusCode::UNAUTHORIZED);
+                }
+                Err(err) => {
+                    // Unable to load user
+                    tracing::warn!(err = %err, uid = %session.uid(), user_uid = ?user_uid, "unable to resolve a valid user");
+                    return return_error(http::StatusCode::INTERNAL_SERVER_ERROR);
+                }
+            };
+
+            tracing::trace!(uid = ?user_uid, "user used");
+            req.extensions_mut().insert(user.clone());
+
+            let res = inner.call(req).await?;
+
+            Ok(res)
+        })
+    }
+}
